3e9b3f8dc079e62b19d3789583630ffd39165a19
Firestart3rr <kuba.wziatka@gmail.com>
2020-02-21 10:20:22 +0100

    added amount of donations

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
 import java.util.List;
@@ -14,15 +16,23 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    private static final String RETURN_LANDING_PAGE = "index";
+
+    private DonationRepository donationRepository;
     private InstitutionRepository institutionRepository;
 
     @ModelAttribute("institutions")
-    public List<Institution> getAllInstitutions(){
+    public List<Institution> getAllInstitutions() {
         return institutionRepository.findAll();
+    }
+
+    @ModelAttribute("donations")
+    public List<Donation> getAllDonations(){
+        return donationRepository.findAll();
     }
 
     @RequestMapping({"/", ""})
     public String homeAction(Model model) {
-        return "index";
+        return RETURN_LANDING_PAGE;
     }
 }
