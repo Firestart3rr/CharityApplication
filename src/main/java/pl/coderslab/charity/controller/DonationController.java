3a9f5dd8da291c0cf0a2857d1bf62b0885091944
Firestart3rr <kuba.wziatka@gmail.com>
2020-02-22 17:21:57 +0100

    donation controller added

---
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -0,0 +1,12 @@
+package pl.coderslab.charity.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/donation")
+public class DonationController {
+}
