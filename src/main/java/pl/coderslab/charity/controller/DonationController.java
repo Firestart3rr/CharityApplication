42808f26b6e30c759fa782ab0634154328b284a8
Firestart3rr <kuba.wziatka@gmail.com>
2020-02-22 20:00:22 +0100

    donation form - step 2

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -3,12 +3,20 @@ package pl.coderslab.charity.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entity.Category;
+import pl.coderslab.charity.entity.Donation;
+import pl.coderslab.charity.entity.Institution;
 import pl.coderslab.charity.repository.CategoryRepository;
+import pl.coderslab.charity.repository.DonationRepository;
+import pl.coderslab.charity.repository.InstitutionRepository;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @AllArgsConstructor
@@ -19,15 +27,29 @@ public class DonationController {
     private static final String SHOW_DONATION_FORM = "form";
 
     private CategoryRepository categoryRepository;
+    private InstitutionRepository institutionRepository;
+    private DonationRepository donationRepository;
 
     @ModelAttribute("categories")
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
 
+    @ModelAttribute("institutions")
+    public List<Institution> getAllInstitutions(){
+        return institutionRepository.findAll();
+    }
+
     @GetMapping("/form")
-    public String showForm(){
+    public String showForm(Model model){
+        Donation donation = new Donation();
+        model.addAttribute("donation", donation);
         return SHOW_DONATION_FORM;
     }
 
+    @PostMapping("/form")
+    public String showForm(Donation donation, Category category, Institution institution, BindingResult result){
+        donationRepository.save(donation);
+        return "index";
+    }
 }
