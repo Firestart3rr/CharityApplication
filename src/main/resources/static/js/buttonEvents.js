5d3d3349d5d791dc9da14504ea1bebd16e4be2fe
Firestart3rr <kuba.wziatka@gmail.com>
2020-03-01 21:00:48 +0100

    interactive button for blocking user. To be corrected.

---
+++ src/main/resources/static/js/buttonEvents.js
@@ -0,0 +1,38 @@
+// $(() =>{
+//     let blockButtonTitle = $('#blockButton').innerText;
+//     $('#blockButton').on('click', ()=> {
+//         if(blockButtonTitle === "Zablokuj użytkownika"){
+//             $('#blockButton').innerText = "Odblokuj użytkownika";
+//         } else if(blockButtonTitle === "Odblokuj użytkownika"){
+//             $('#blockButton').innerText = "Zablokuj użytkownika";
+//         }
+//     })
+// })
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     document
+//         .querySelectorAll('#blockButton')
+//         .forEach((el) => {
+//             el.addEventListener("click", function () {
+//                 let blockButtonTitle = document.querySelectorAll("#blockButton").innerText.toString();
+//                 if (typeof blockButtonTitle == "Zablokuj użytkownika") {
+//                     document.querySelectorAll('#blockButton').Text = "Odblokuj użytkownika";
+//                 } else if (typeof blockButtonTitle == "Odblokuj użytkownika") {
+//                     document.querySelectorAll('#blockButton').Text = "Zablokuj użytkownika";
+//                 }
+//             })
+//         })
+// })
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("blockButton").onclick=function(){
+        let buttonTitle = document.getElementById("blockButton").innerText
+        // document.getElementById("blockButton").innerText("Odblokuj");
+        if(buttonTitle == "Odblokuj użytkownika"){
+            this.innerText="Zablokuj użytkownika"
+        } else if(buttonTitle == "Zablokuj użytkownika"){
+            this.innerText="Odblokuj użytkownika"
+        }
+    }
+})
