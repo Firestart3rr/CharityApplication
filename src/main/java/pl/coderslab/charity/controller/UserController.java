80a46e81464c4c7b19d0be9e5f13909c3a3304e2
Firestart3rr <kuba.wziatka@gmail.com>
2020-03-02 12:29:25 +0100

    added user profile view

--- src/main/java/pl/coderslab/charity/controller/UserController.java
+++ src/main/java/pl/coderslab/charity/controller/UserController.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entity.AppUser;
 import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.entity.Institution;
 import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
+import pl.coderslab.charity.repository.UserRepository;
+import pl.coderslab.charity.service.UserServiceImpl;
 
 import java.util.List;
 
@@ -22,9 +25,12 @@ import java.util.List;
 public class UserController {
 
     private static final String RETURN_USER_INDEX_PAGE = "userIndex";
+    private static final String RETURN_USER_PROFILE = "user/userProfile";
 
     private final DonationRepository donationRepository;
     private final InstitutionRepository institutionRepository;
+    private final UserServiceImpl userService;
+    private final UserRepository userRepository;
 
     @ModelAttribute("institutions")
     public List<Institution> getAllInstitutions() {
@@ -38,11 +44,14 @@ public class UserController {
 
     @GetMapping("")
     public String showUserIndexPage(Model model) {
-//        CurrentUser currentUser = new CurrentUser();
-        AppUser appUser = new AppUser();
         model.addAttribute("sumOfDonatedBags", donationRepository.sumOfAllDonatedBags());
-//        model.addAttribute("appUser", appUser);
         return RETURN_USER_INDEX_PAGE;
+    }
+
+    @GetMapping("/profile/{email}")
+    public String showUserData(Model model, @PathVariable String email){
+        model.addAttribute("appUser", userRepository.findAppUserByEmail(email));
+        return RETURN_USER_PROFILE;
     }
 
 
